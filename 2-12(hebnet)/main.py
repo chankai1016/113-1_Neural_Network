c3c1ddceb1b26d19fe19fecfe517fe708f095985
Kai Chan, J. <chankai1016@gmail.com>
2024-10-24 00:49:44 +0800

    Second Commit

--- 2-12(hebnet)/main.py
+++ 2-12(hebnet)/main.py
@@ -1,58 +1,41 @@
 def heb_net(x, target, alpha, init):
     i = 0
     w = list()
-
-    # first time
-    print("#{}".format(1), end=", ")
-    print("P_1 = {}, w = {}".format(x[0], init), end=", ")
-    net = wx(init, x[0])  # net value
-    print("net = {}".format(net), end=", ")
-    print("f(net) = {}, target = {}".format(f_net(net), target[0]))
-    if f_net(net) == target[0]:  # y = t ?
-        w.append(list(init))
-        print("Result: do nothing.   {}".format(w[0]))
-    else:
-        w_new_list = list(w_new(init, alpha, target[0], f_net(net), x[0]))
-        w.append(list(w_new_list))
-        print("Result: weight change {}".format(w[0]))
-
-    i += 1
-
-    # second time -
+    w.append(list(init))
     while True:
         print("#{}".format(i + 1), end=", ")
-        print("P_{} = {}, w = {}".format((i % len(x)) + 1,x[i % len(x)], w[i - 1]), end=", ")
-        net = wx(w[i - 1], x[i % len(x)])  # net value
+        print(
+            "P_{} = {}, w = {}".format((i % len(x)) + 1, x[i % len(x)], w[i]), end=", "
+        )
+        net = wx(w[i], x[i % len(x)])  # net value
         print("net = {}".format(net), end=", ")
         print("f(net) = {}, target = {}".format(f_net(net), target[i % len(x)]))
         if f_net(net) == target[i % len(x)]:  # y = t ?
-            w.append(list(w[i - 1]))
-            print("Result: do nothing.   {}".format(w[i]))
+            w.append(list(w[i]))
+            print("Result: do nothing.   {}".format(w[i + 1]))
         else:
             w_new_list = list(
-                w_new(w[i - 1], alpha, target[i % len(x)], f_net(net), x[i % len(x)])
+                w_new(w[i], alpha, target[i % len(x)], f_net(net), x[i % len(x)])
             )
             w.append(w_new_list)
-            print("Result: weight change {}".format(w[i]))
+            print("Result: weight change {}".format(w[i + 1]))
         if i >= len(x):
             check = 0
             for j in range(len(x)):
-                if w[i] == w[i - j]:
+                if w[i + 1] == w[i - j]:
                     check += 1
             if check >= len(x):
                 print("##")
-                print("倒數{}次計算結果等值，即weight = {}".format(len(x),w[i]))
-                # for j in range(check):
-                #     if j == check - 1:
-                #         print()
-                #     else:
-                #         print(w[i - j], end=", ")
+                print(
+                    "倒數{}次計算結果等值，最終結果：weight = {}".format(len(x), w[i])
+                )
                 print()
                 break
             if i >= 100 - 1:
                 print("## 執行100次")
                 break
         i += 1
+
 
 def w_new(w_old, alpha, t, y, x):
     # w_new = w_old + alpha * (t - y) * x
